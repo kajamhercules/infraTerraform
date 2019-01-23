147cb00ac8a87b4a720d168a50e1c9a83e64fca0
Alex <kajam.hercules@gmail.com>
2019-01-23 11:39:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,7 @@ provider "azurerm" {
   
   
   subscription_id             = "66391ae6-a4d7-4401-a643-245509f5c337"
-  #client_id                   = "fa90cebf-8a20-4372-bfe5-36e78059beeb"
-  client_id                   ="16a85977-f3d2-4f8f-8396-66afa8b03eba"
+  client_id                   = "fa90cebf-8a20-4372-bfe5-36e78059beeb"
   client_certificate_path     = "service-principal.pfx"
   client_certificate_password = "Qwerty123456"
   tenant_id                   = "bd8ab44a-b4f0-4055-b414-10d1a87c1666"
