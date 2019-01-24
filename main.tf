52158e90212b8d0a0bfb3d5525919f5024743a33
Alex <kajam.hercules@gmail.com>
2019-01-24 14:27:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
   #client_id                   = "a14f94ef-6c2a-4a1f-a7cc-ea23ad338c87"
   #client_certificate_password = "1KFvffN2jZWb6nxmCvpBwOI0s16Owgr83EYZ4jVmGfI="
   #tenant_id                   = "bd8ab44a-b4f0-4055-b414-10d1a87c1666"
-  
+  #########
   
   subscription_id             = "66391ae6-a4d7-4401-a643-245509f5c337"
   client_id                   = "fa90cebf-8a20-4372-bfe5-36e78059beeb"
